a43f6c9d31e49b32c35d403ede610dbd4bc56f59
Stuart Ort <stuart.ort@lexisnexisrisk.com>
2022-08-04 15:13:32 -0400

    change version for test

--- version.cmake
+++ version.cmake
@@ -2,7 +2,7 @@
 ##  Version Information
 ###
 set ( HPCC_NAME "Community Edition" )
-set ( HPCC_PROJECT "community" )
+set ( HPCC_PROJECT "STU" )
 set ( HPCC_MAJOR 8 )
 set ( HPCC_MINOR 8 )
 set ( HPCC_POINT 3 )
